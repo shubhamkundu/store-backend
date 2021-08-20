85a2f5999183681ea993f7a8826d9e35cd083aa7
Shubham Kundu <shbhmknd@gmail.com>
2021-08-20 14:40:22 +0530

    fixed file names in import

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 module.exports = ({ db }) => ({
     adminRouter: require('./admin.router')({ db }),
     storeRouter: require('./store.router')({ db }),
-    userRouter: require('./sub-user.router')({ db }),
+    userRouter: require('./user.router')({ db }),
     productRouter: require('./product.router')({ db })
 });
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 module.exports = ({ db }) => ({
     adminService: require('./admin.service')({ db }),
     storeService: require('./store.service')({ db }),
-    userService: require('./sub-user.service')({ db }),
+    userService: require('./user.service')({ db }),
     productService: require('./product.service')({ db })
 });
